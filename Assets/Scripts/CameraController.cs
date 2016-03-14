4de8893d72509164b501f5d46bcc3fbcf1f4c649
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-03-14 19:29:05 -0400

    Added screenshots

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    Vector3 lastMousePos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lastMousePos == null) lastMousePos = Input.mousePosition;
+        Vector3 mouseDeltaPos = Input.mousePosition - lastMousePos;
+        if (Input.GetMouseButton(0)) {
+            //transform.RotateAround()
+        }
+        lastMousePos = Input.mousePosition;
+	}
+}
